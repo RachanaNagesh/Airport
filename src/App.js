0614187287420f56f3108b6dbd37706d3daa340c
RachanaNagesh <rachana.nov12@gmail.com>
2026-01-20 23:21:00 +1100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { IncidentSummary } from "./Pages/IncidentSummary";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<IncidentSummary />
+			</header>
+		</div>
+	);
+}
+
+export default App;
